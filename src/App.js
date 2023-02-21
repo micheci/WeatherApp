7963661739964c148eee8ea12b441045a95ceb0c
micheci <michecimartinez@gmail.com>
2023-02-21 00:42:03 -0600

    added display to client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from 'react'
+
+
 
 function App() {
+  const [data,setData]=useState([{name:"default"}])
+
+  const fetchData=()=>{
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '67d628f24dmsh30640eb40d010d8p1e1b4cjsn0d847e304a46',
+        'X-RapidAPI-Host': 'dark-sky.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://dark-sky.p.rapidapi.com/44,-96?units=auto&lang=en', options)
+      .then(response => response.json())
+      .then(response => console.log(response.currently))
+      .catch(err => console.error(err));}
+
+  useEffect(() => {
+    fetchData()
+}, []);
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='card'>
+          <h4>Location</h4>
+          <h2>{data.temperature}</h2>
+          
+          <div className='flex'>
+          <p>{data.humidity}</p>
+          <p>{data.summary}</p>
+          <p>{data.windSpeed}</p>
+          </div>
+
+         
+          <p>{data.time}</p>
+          
+        </div>
       </header>
+      
     </div>
   );
 }
