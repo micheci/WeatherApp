a5a8f68369a1660cc299617fc3d68f5eb424392a
micheci <michecimartinez@gmail.com>
2023-02-25 10:41:05 -0600

    add a button to show details

--- src/components/temp.js
+++ src/components/temp.js
@@ -6,13 +6,18 @@ function Temp() {
     const [data,setData]=useState();
 
     useEffect(()=>{
-        fetch('http://localhost:4001').then(response=>{
+        fetch('http://localhost:4001')
+        .then(response=>{
             setData(response.json())
         })
     },[])
 
   return (
-    <div>{data || 0}</div>
+    <>
+    <div>{console.log(1)}</div>
+    <div>test</div>
+   
+    </>
   )
 }
 
